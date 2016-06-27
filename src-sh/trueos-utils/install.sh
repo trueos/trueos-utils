5c89babae243efca64729cd7e9917fe9ce5cc760
Joe Maloney <pkgdemonteam@gmail.com>
2016-06-27 15:09:51 -0400

    Import trueos-utils

---
+++ src-sh/trueos-utils/install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+DEFAULT="/usr/local"
+
+if [ -z "$1" ] ; then 
+	LB="${DEFAULT}" 
+else
+	LB="${1}" 
+fi
+
+if [ "$LB" = "DOMAKE" ] ; then
+  INSTARGET=""
+elif [ "$LB" = "DOCLEAN" ] ; then
+  INSTARGET="clean"
+else
+  INSTARGET="install"
+fi
+
+DIR=`dirname $0`
+DIR=`realpath $DIR`
+cd ${DIR}
+
+SUBPROJ="about pc-checkxdisplay pc-firstgui pc-installdialog \
+	 pc-su pc-systemflag"
+
+# Add all the sub-projects
+for i in ${SUBPROJ}
+do
+  cd $i
+  if [ $? -ne 0 ] ; then exit 1 ; fi
+  make ${INSTARGET}
+  if [ $? -ne 0 ] ; then exit 1 ; fi
+  cd ${DIR}  
+  if [ $? -ne 0 ] ; then exit 1 ; fi
+done
